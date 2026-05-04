e89e089cc4c993956479b4774a074cc60bf5cf56
alonax <46304073+alonax@users.noreply.github.com>
2026-05-04 15:25:03 +0300

    added uvicorn

--- Chapter 4 - Devops & Devlopment/assets/pizza_api_project/main.py
+++ Chapter 4 - Devops & Devlopment/assets/pizza_api_project/main.py
@@ -1,6 +1,6 @@
 
 from fastapi import FastAPI
-
+import uvicorn
 from pizza_api_project.models.pizza import PizzaItem, OrderRequest
 from pizza_api_project.routers import orders
 from pizza_api_project.routers.orders import create_order
@@ -16,6 +16,7 @@ def root():
 
 
 def main():
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
     item = {"name": "cornpizza", "price": "4.5"}
     items: PizzaItem = []
     pizzaitem1 = PizzaItem(**item)
