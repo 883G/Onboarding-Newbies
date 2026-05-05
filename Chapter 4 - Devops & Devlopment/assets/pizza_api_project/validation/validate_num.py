68d75963972e7cb5c494f8e71f780bca6939484b
alonax <46304073+alonax@users.noreply.github.com>
2026-05-05 11:59:44 +0300

    delete unnecisery validation python package

--- Chapter 4 - Devops & Devlopment/assets/pizza_api_project/validation/validate_num.py
+++
@@ -1,2 +0,0 @@
-def num_is_bigger_then_zero(num: float) -> bool:
-    return num > 0
--- Chapter 4 - Devops & Devlopment/assets/pizza_api_project/validation/validate_str.py
+++
@@ -1,6 +0,0 @@
-import re
-
-def clean_str_from_spicel_characters(string: str) -> str:
-    # Removes all special characters except spaces and alphanumeric characters
-    cleaned = re.sub(r'[^a-zA-Z0-9\s]', '', string)
-    return cleaned
