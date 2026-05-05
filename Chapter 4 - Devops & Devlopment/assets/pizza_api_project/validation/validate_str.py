b312f04377e90098ef0d6161b1659315c647a1b4
alonax <46304073+alonax@users.noreply.github.com>
2026-05-05 11:10:40 +0300

    added clean_str function that validate the str

---
+++ Chapter 4 - Devops & Devlopment/assets/pizza_api_project/validation/validate_str.py
@@ -0,0 +1,6 @@
+import re
+
+def clean_str(string: str) -> str:
+    # Removes all special characters except spaces and alphanumeric characters
+    cleaned = re.sub(r'[^a-zA-Z0-9\s]', '', string)
+    return cleaned
