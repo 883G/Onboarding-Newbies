3806cca212df7793c30ae1fc2f8468f9896a0bee
alonax <46304073+alonax@users.noreply.github.com>
2026-05-06 15:53:15 +0300

    added test_post_orders_endpoint func, THERES ERROR item_pizza_tests.py - pydantic_core._pydantic_core.ValidationError: 1 validation error for OrderRequest

--- Chapter 4 - Devops & Devlopment/assets/pizza_api_project/tests/item_pizza_tests.py
+++ Chapter 4 - Devops & Devlopment/assets/pizza_api_project/tests/item_pizza_tests.py
@@ -1,23 +1,30 @@
-from unittest.mock import patch
+from unittest.mock import patch, Mock
+
+from fastapi import HTTPException
 
 import pizza_api_project
 import pizza_api_project.db_handler.database_orm
+from pizza_api_project.models.pizza import OrderRequest
 from pizza_api_project.models.pizza_order import PizzaOrder
+from pizza_api_project.routers.orders import create_order
 from pizza_api_project.tests.test_main import client
 
 
 def test_post_order() -> None:
-    #response = client.post("/orders")
-    #assert response.status_code == 400
+    # response = client.post("/orders")
+    # assert response.status_code == 400
     pass
+
+
 def test_get_menu() -> None:
     response = client.get("/menu")
     assert response.status_code == 200
     assert len(response.json()) == 3
     assert response.json()[0]["name"] == "Margherita"
 
-@patch('pizza_order.PizzaOrder')
-def test_save_order_to_db_func(mock_save_order_to_db):
-    mock_save_order_to_db.return_value = True
-    order_data = {'customer_name': 'ofek', 'pizzas': [{"name": "Margherita", "price": 10.0}]}
-    assert pizza_api_project.db_handler.database_orm.save_order_to_db(order_data) is True
+
+def test_post_orders_endpoint():
+    mock_order_request_without_items = Mock()
+    mock_order_request_without_items.pizzas = []
+    assert create_order(mock_order_request_without_items) == HTTPException(status_code=400,
+                                                                           detail="Items list is empty")
--- Chapter 4 - Devops & Devlopment/assets/pizza_api_project/tests/test_main.py
+++ Chapter 4 - Devops & Devlopment/assets/pizza_api_project/tests/test_main.py
@@ -4,19 +4,28 @@ from fastapi.testclient import TestClient
 from unittest.mock import patch, Mock
 
 import pizza_api_project.db_handler.database_orm
+from conf import order_request_test_data
 from pizza_api_project.main import app
 from pizza_api_project.models.pizza import OrderRequest
 from pizza_api_project.models.pizza_order import PizzaOrder
+from fastapi import HTTPException
 
 client = TestClient(app)
 
 
 @patch('pizza_api_project.db_handler.database_orm.save_order_to_db', return_value=True)
-def test_save_order_to_db(mock_save_order_to_db_func):
+def test_save_order_func(mock_save_order_to_db_func):
     order_data = {'customer_name': 'ofek', 'pizzas': [{"name": "Margherita", "price": 10.0}]}
     order_request: OrderRequest = OrderRequest(**order_data)
     pizza_order: PizzaOrder = PizzaOrder(order_request)
     assert pizza_order.save_order() is True
+
+
+@patch('pizza_api_project.models.pizza.OrderRequest', return_value=OrderRequest(**order_request_test_data))
+def test_post_orders_endpoint(mock_order_request_without_items):
+    response = client.post("/orders")
+    assert response.status_code == 400
+    assert response.detail == "Items list is empty"
 
 
 # ==========================================
