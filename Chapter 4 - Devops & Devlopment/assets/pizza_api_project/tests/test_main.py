8efe8af8788691e963b013fdf82a5370006f2c65
alonax <46304073+alonax@users.noreply.github.com>
2026-05-06 18:13:42 +0300

    adding comments to the tests

--- Chapter 4 - Devops & Devlopment/assets/pizza_api_project/tests/test_main.py
+++ Chapter 4 - Devops & Devlopment/assets/pizza_api_project/tests/test_main.py
@@ -1,45 +1,46 @@
 ﻿from unittest import mock
 
 from fastapi.testclient import TestClient
-from unittest.mock import patch, Mock
-
-import pizza_api_project.db_handler.database_orm
-
+from unittest.mock import patch
 from pizza_api_project.main import app
 from pizza_api_project.models.pizza import OrderRequest
 from pizza_api_project.models.pizza_order import PizzaOrder
-from fastapi import HTTPException
 
 client = TestClient(app)
 
+
+# Arrange
 @patch('pizza_api_project.db_handler.database_orm.save_order_to_db', return_value=True)
 def test_success_save_order_func(mock_save_order_to_db_func):
     order_data = {'customer_name': 'ofek', 'pizzas': [{"name": "Margherita", "price": 10.0}]}
     order_request: OrderRequest = OrderRequest(**order_data)
     pizza_order: PizzaOrder = PizzaOrder(order_request)
+    # Assert
     assert pizza_order.save_order() is True
 
+
+# Arrange
 @patch('pizza_api_project.models.pizza_order.PizzaOrder.the_items_list_is_empty', return_value=True)
 def test_fail_post_order_endpoint(mock_order) -> None:
+    # Act
     response = client.post("/orders", json={'customer_name': 'ofek', 'pizzas': []})
+    # Assert
     assert response.status_code == 400
 
 
+# Arrange
 @patch('pizza_api_project.models.pizza_order.PizzaOrder.the_items_list_is_empty', return_value=False)
 def test_success_post_order_endpoint(mock_order) -> None:
+    # Act
     response = client.post("/orders", json={'customer_name': 'ofek', 'pizzas': [{"name": "Margherita", "price": 10.0}]})
+    # Assert
     assert response.status_code == 200
 
+
 def test_get_menu() -> None:
+    # Act
     response = client.get("/menu")
     assert response.status_code == 200
     assert len(response.json()) == 3
     assert response.json()[0]["name"] == "Margherita"
 
-# Example of what is needed:
-# @patch('main.save_order_to_db')
-# def test_create_order_success(mock_save_db):
-#     # 1. Arrange: setup mock return value and payload
-#     # 2. Act: send POST request to /orders
-#     # 3. Assert: check status code, response data, and that mock was called
-#     pass
